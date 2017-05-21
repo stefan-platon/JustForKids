eef6f5d63272f47c9228e5e306deeba3ee209060
Stefan Platon <platonfanica@yahoo.com>
2017-05-21 16:23:21 +0300

    Adaugat mai multe functionalitati paginii insereaza_domeniu
    Posibilitatea de a face upload unei poze

--- ADMIN/BACK/admin_insereaza_domeniu.php
+++ ADMIN/BACK/admin_insereaza_domeniu.php
@@ -4,15 +4,44 @@ $sql = 'begin :rezultat :=  admin_pachet.insert_domeniu(:v_nume,:v_tip, :v_img);
 $stid = oci_parse($connection, $sql);
 oci_bind_by_name($stid, ":v_nume", $_POST["nume"]);
 oci_bind_by_name($stid, ":v_tip", $_POST["tip"]);
-oci_bind_by_name($stid, ":v_img", $_POST["img"]);
-oci_bind_by_name($stid, ':rezultat', $rezultat, 100);
-if(!oci_execute($stid))
-{
-    $e = oci_error($stid);
-    echo "Something went wrong :( <br/>";
-    echo "Error: " . $e['message'];
+$target_dir = "../IMG/DOMENII/";
+$info = pathinfo($_FILES['fileToUpload']['name']);
+$ext = $info['extension']; // get the extension of the file
+$newname = $_POST["nume"].".".$ext;
+$target_file = $target_dir . $newname;
+$uploadOk = 1;
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
 }
-oci_free_statement($stid);
-oci_close($connection);
-echo $rezultat;
+// Allow certain file formats
+if($ext != "jpg" && $ext != "png" && $ext != "jpeg") {
+    echo "Sorry, only JPG, and PNG files are allowed.";
+    $uploadOk = 0;
+}
+if ($uploadOk == 0) {
+    echo "Sorry, your picture was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        oci_bind_by_name($stid, ":v_img", $target_file);
+        oci_bind_by_name($stid, ':rezultat', $rezultat, 100);
+        if(!oci_execute($stid))
+        {
+            $e = oci_error($stid);
+            echo "Something went wrong :( <br/>";
+            echo "Error: " . $e['message'];
+        }
+        oci_free_statement($stid);
+        oci_close($connection);
+        echo $rezultat;
+    } else {
+        echo "Sorry, there was an error uploading your picture.";
+    }
+}
 ?>
