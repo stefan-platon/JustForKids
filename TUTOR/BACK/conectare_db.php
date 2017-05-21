ce7e3b80e0071d975ad3cf2bb3ac632da679d66e
stefan-platon <platonfanica@yahoo.com>
2017-05-21 15:43:46 +0300

    Create conectare_db.php

---
+++ TUTOR/BACK/conectare_db.php
@@ -0,0 +1,16 @@
+<?php
+//Oracle DB user name
+$username = 'STUDENT';
+// Oracle DB user password
+$password = 'STUDENT';
+// Oracle DB connection string
+$connection_string = 'localhost/xe';
+//Connect to an Oracle database
+$connection = oci_pconnect(
+$username,
+$password,
+$connection_string
+);
+if(!$connection)
+    echo "Conexiune esuata.";
+?>
