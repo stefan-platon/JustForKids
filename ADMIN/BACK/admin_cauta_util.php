7f40cd8d73b99a565410f8c1f02cd2eadb31a8aa
stefan-platon <platonfanica@yahoo.com>
2017-05-25 11:35:25 +0300

    Schimbat interogarea pentru a beneficia de index

--- ADMIN/BACK/admin_cauta_util.php
+++ ADMIN/BACK/admin_cauta_util.php
@@ -5,7 +5,7 @@
 
         $flag=true;
         if ($_POST["uname"]!=null && $_POST['uid']==0) {
-            $stid = oci_parse($connection, "SELECT player_id,first_name,last_name,username,email FROM player where username like '%".$_POST["uname"]."%'");
+            $stid = oci_parse($connection, "SELECT player_id,first_name,last_name,username,email FROM player where LOWER(username) = ".$_POST["uname"]);
             if(!oci_execute($stid))
             {
                 $error_flag = 0;
