f9fbf1b6904ac193748ed68c2d5583eef82ee58a
Catalin Dragomirescu <catalin11gb@gmail.com>
2017-05-29 12:52:40 +0300

    Update/improve user stats PHP

--- PLAYER/BACK/user_stats.php
+++ PLAYER/BACK/user_stats.php
@@ -5,64 +5,62 @@
  * Date: 24-May-17
  * Time: 01:29
  */
+$player_id = $_SESSION["player_id"];
 $username = $_SESSION["username"];
-$stid = oci_parse($connection, 'SELECT sum(t.score) FROM tests t join player p on t.player_id = p.player_id where p.username = :username');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'SELECT sum(total_score) FROM player_stats where player_id = :player_id');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
 echo '<tr class="profil_tr">' . '<td class="profil_td">Scorul total:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'SELECT to_char(min(pa.logged_in),\'DD.MM.YYYY\') FROM player_activity pa join player p on pa.player_id = p.player_id where p.username = :username');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'SELECT to_char(min(logged_in),\'DD.MM.YYYY\') FROM player_activity where player_id = :player_id');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
-echo '<tr class="profil_tr">' . '<td class="profil_td">Prima logare:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
+echo '<tr class="profil_tr">' . '<td class="profil_td">Jucator JfK din:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'select domain, x from (select d.domain_name as domain, sum(t.score) as x from domains d join tests t on d.domain_id = t.domain_id join player p on t.player_id = p.player_id
-            where p.username = :username group by d.domain_name order by 2 desc) where rownum=1');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'SELECT d.DOMAIN_NAME, p.total_score from PLAYER_STATS p join DOMAINS d on p.DOMAIN_ID = d.DOMAIN_ID where p.PLAYER_ID = :player_id order by p.total_score desc');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
-echo '<tr class="profil_tr">' . '<td class="profil_td">Domeniul cu scorul cel mai mare:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
+echo '<tr class="profil_tr">' . '<td class="profil_td">Domeniul cu scorul cel mai mare:</td>' . '<td class="profil_td">' . $row[0] . ' (' . $row[1] . ')</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'select domain, x from (select d.domain_name as domain, sum(t.score) as x from domains d join tests t on d.domain_id = t.domain_id join player p on t.player_id = p.player_id
-            where p.username = :username group by d.domain_name order by 2) where rownum=1');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'SELECT d.DOMAIN_NAME, p.total_score from PLAYER_STATS p join DOMAINS d on p.DOMAIN_ID = d.DOMAIN_ID where p.PLAYER_ID = :player_id order by p.total_score');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
-echo '<tr class="profil_tr">' . '<td class="profil_td">Domeniul cu scorul cel mai mic:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
+echo '<tr class="profil_tr">' . '<td class="profil_td">Domeniul cu scorul cel mai mic:</td>' . '<td class="profil_td">' . $row[0] . ' (' . $row[1] . ')</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'SELECT round(sum(t.score)/count(t.score)*10, 2) FROM tests t join player p on t.player_id = p.player_id where p.username = :username');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'SELECT round(sum(score)/count(score)*10, 2) FROM tests where PLAYER_ID = :player_id');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
 echo '<tr class="profil_tr">' . '<td class="profil_td">Procentajul de raspunsuri corecte:</td>' . '<td class="profil_td">' . $row[0] . '%</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'select count(t.test_id) from tests t join player p on t.player_id = p.player_id where p.username = :username');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'select sum(number_of_plays) from player_stats where PLAYER_ID = :player_id group by player_id');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
 echo '<tr class="profil_tr">' . '<td class="profil_td">Numarul de teste:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'select count(t.test_id) from tests t join player p on t.player_id = p.player_id where p.username = :username and t.score >= 5');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'select count(test_id) from tests where PLAYER_ID = :player_id and score >= 5');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
 echo '<tr class="profil_tr">' . '<td class="profil_td">Numarul de teste cu nota peste 5:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
 oci_free_statement($stid);
 
-$stid = oci_parse($connection, 'select count(t.test_id) from tests t join player p on t.player_id = p.player_id where p.username = :username and t.score < 5');
-oci_bind_by_name($stid, ':username', $username);
+$stid = oci_parse($connection, 'select count(test_id) from tests where PLAYER_ID = :player_id and score < 5');
+oci_bind_by_name($stid, ':player_id', $player_id);
 oci_execute($stid);
 $row = oci_fetch_array($stid, OCI_BOTH);
 echo '<tr class="profil_tr">' . '<td class="profil_td">Numarul de teste cu nota sub 5:</td>' . '<td class="profil_td">' . $row[0] . '</td></tr>';
 oci_free_statement($stid);
-
 
 oci_close($connection);
