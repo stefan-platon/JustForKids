6c7cc38a544541be99e9c924f64f5d866f571ba1
Dimache Alexandru <dimache.alexandru96@gmail.com>
2017-05-31 23:49:40 +0300

    test_variante.php, redirect_to_final_page.php
    test_variante.php este complet dar trebuie modificate mai incolo caile catre celelalte fisiere pentru a respecta structura proiectului final. Redirectul nu este complet, l-am folosit pentru verificare

--- PLAYER/BACK/redirect_to_final_page.php
+++ PLAYER/BACK/redirect_to_final_page.php
@@ -1,7 +1,12 @@
 <?php
 
 session_start();
-for($i=0;$i<10;$i++)
-    echo $_POST['q'+$i];
+$answers=json_decode($_POST['answers']);
+for($i=0;$i<10;$i++){
+    echo "La intrebarea:<br>";
+    echo $_SESSION['test'][0][$i][1].'<br>';
+    echo "Ati raspuns: ".$answers[$i].'<br>';
+    echo "Raspunsul corect era: ".$_SESSION['test'][1][$i][3].'<br>';
+}
 
 ?>
