2577096dc1e765a49cf9bb171d6476569926547c
Catalin Dragomirescu <catalin11gb@gmail.com>
2017-06-03 22:03:32 +0300

    Add tutor_statistics functionality

--- TUTOR/BACK/tutor_statistics.php
+++ TUTOR/BACK/tutor_statistics.php
@@ -15,13 +15,11 @@ $players_id = array();
 $stid = oci_parse($connection, 'select LAST_NAME, FIRST_NAME, PLAYER_ID from player where TUTOR_ID = :tutor_id');
 oci_bind_by_name($stid, ':tutor_id', $tutor_id);
 oci_execute($stid);
-$row = oci_fetch_array($stid, OCI_BOTH);
 while (($row = oci_fetch_array($stid, OCI_BOTH)) != false) {
     array_push($players, $row[1] . ' ' . $row[0]);
     array_push($players_id, $row[2]);
 }
 oci_free_statement($stid);
-
 
 
 echo '<div class="container_tutor_statistics">
@@ -30,7 +28,7 @@ echo '<div class="container_tutor_statistics">
 $_SESSION["tplayer_id"] = $players_id;
 for ($i = 0; $i < count($players); $i++) {
     echo '<div class="player">';
-    echo '<input type="submit" id= "'.$i.'" value="'.$players[$i].'" class="player_button" style="">';
+    echo '<input type="submit" id= "'.$i.'" value="'.$players[$i].'" name="name" class="player_button" style="">';
     echo '</div>';
 }
 echo '<div class="filler"></div>';
