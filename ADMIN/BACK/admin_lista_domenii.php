defd3b972eed7bd19034b0f4ea666c36aed731c6
Dimache Alexandru <dimache.alexandru96@gmail.com>
2017-06-09 22:54:04 +0300

    securitate III

--- ADMIN/BACK/admin_lista_domenii.php
+++ ADMIN/BACK/admin_lista_domenii.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+if($_SESSION['secret']!=$_POST['secret'])
+    header("Location:../FRONT/HTML/session_error.html");
+else
+    session_write_close();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     include("conectare_db.php");
