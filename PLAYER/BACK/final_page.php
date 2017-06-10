be10374e9b03643bf5b616e33fe2526e544899e9
Dimache Alexandru <dimache.alexandru96@gmail.com>
2017-06-10 03:14:11 +0300

    bug

--- PLAYER/BACK/final_page.php
+++ PLAYER/BACK/final_page.php
@@ -4,9 +4,9 @@ session_start();
 if ($_SESSION['secret'] != $_POST['secret'])
     header('Location: ../../../INTRO/FRONT/HTML/session_error.html');
 if (!$_SESSION['online'] === true || !$_SESSION['rights'] == 'player')
-    header('Location: ../../../INTRO/FRONT/HTML/logged_user_frame.html');
+    header('Location: ../../../INTRO/FRONT/HTML/login_frame.html');
 if($_SESSION['last_page']!="redirect_to_final_page.php" && $_SESSION['last_page']!="final_page.php")
-    header('Location: ../../../INTRO/FRONT/HTML/logged_user_frame.html');
+    header('Location: ../FRONT/HTML/logged_user_frame.html');
 
 //deschid sesiunea si incarc raspunsurile date de utilizator
 $answers = $_SESSION['userAnswers'];
